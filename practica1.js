4ab1cf06cf32dfe6e3167ccfa874c1b5e3852388
emmuriel <emarmur108@g.educaand.es>
2022-02-14 19:04:14 +0100

    okcopy paste

--- practica1.js
+++ practica1.js
@@ -1,32 +1,35 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
     //No permitir copiar ni pegar en los campos contraseña
-    document.querySelector('#pwd1').addEventListener("copy",(evt)=>{
-
+    document.querySelector('#tx_pwd1').addEventListener("copy",(evt)=>{
+        alert("No se puede copiar/pegar en eel campo contraseña");
         evt.preventDefault();
 
     });
-    document.querySelector('#pwd1').addEventListener("paste",(evt)=>{
+    document.querySelector('#tx_pwd1').addEventListener("paste",(evt)=>{
+        alert("No se puede copiar/pegar en eel campo contraseña");
         evt.preventDefault();
     });
 
-    document.querySelector('#pwd2').addEventListener("copy",(evt)=>{
-
+    document.querySelector('#tx_pwd2').addEventListener("copy",(evt)=>{
+        alert("No se puede copiar/pegar en eel campo contraseña");
         evt.preventDefault();
+        
 
     });
-    document.querySelector('#pwd2').addEventListener("paste",(evt)=>{
+    document.querySelector('#tx_pwd2').addEventListener("paste",(evt)=>{
+        alert("No se puede copiar/pegar en el campo contraseña");
         evt.preventDefault();
     });
 
     //Validaciones de los input
-    document.querySelector('#pwd1').addEventListener("input",(evt)=>{
+    document.querySelector('#tx_pwd1').addEventListener("input",(evt)=>{
         //validar contraseña valida con checkvalidity
 
 
 
     });
-    document.querySelector('#pwd2').addEventListener("input",(evt)=>{
+    document.querySelector('#tx_pwd2').addEventListener("input",(evt)=>{
         //validar contraseña valida con checkvalidity
 
         
@@ -47,9 +50,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 });
 
-function validarFormulario(){
-    let error;
+function okFormulario(){
+    let ok=true;
 
+    //Valida campo email.
+    document.querySelector("#tx_pdw1").textContent;
 
-    return error;
+    //valida que las contraseñas sean iguales
+    let pwd1= document.querySelector("#tx_pdw1").textContent;
+    let pwd2= document.querySelector("#tx_pdw2").textContent;
+        if (pwd1!=pwd2){ 
+            document.querySelector("#err_pdw").setAttribute("hidden", "" );
+            ok=false;
+        }
+
+    return ok;
 }
